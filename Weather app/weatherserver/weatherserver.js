ac185e56ce87a0491596fb9dd873e7f98440ebb9
ayonization <62653441+ayonization@users.noreply.github.com>
2020-05-15 14:15:32 +0530

    Add forecasts

--- Weather app/weatherserver/weatherserver.js
+++ Weather app/weatherserver/weatherserver.js
@@ -1,8 +1,9 @@
 const request = require('request');
 
+
 var getweather=(lat,lng,callback)=>{
     request({
-        url:'http://api.openweathermap.org/data/2.5/weather?lat='+lat+'&lon='+lng+'&appid=3808be54f88838d3ba23d501dae139f8&units=metric',
+        url:'https://api.openweathermap.org/data/2.5/onecall?lat='+lat+'&lon='+lng+'&%20exclude=&appid=668599a16c8f0c65e9a39da4940aeae3&units=metric',
         json:true
     },(error,response,body)=>{
         if(error)
@@ -10,19 +11,24 @@ var getweather=(lat,lng,callback)=>{
            callback("Cannot connect to the weather server");
             
         }
-        else if(body.cod!=200)
-        {
-            callback("Invalid address");
+        // else if(body.cod!=200)
+        // {
+        //     callback("Invalid address");
             
-        }
+        // }
         else{
         callback(undefined,{
-            temperature:body.main.temp,
-            feelslike:body.main.feels_like,
-            humidity:body.main.humidity,
-            windspeed:body.wind.speed,
-            weatheris:body.weather[0].main,
-            weatherdescrip:body.weather[0].description,
+            sunrise:body.current.sunrise,
+            sunset:body.current.sunset,
+            temp:body.current.temp,
+            humidity:body.current.humidity,
+            weatherdescrip:body.current.weather[0].main,
+            tomaxtemp:body.daily[0].temp.max,
+            tomintemp:body.daily[0].temp.max,
+            toweather:body.daily[0].weather[0].main,
+            nextmaxtemp:body.daily[1].temp.max,
+            nextmintemp:body.daily[1].temp.min,
+            nextweather:body.daily[1].weather[0].main
         });
         }
     
